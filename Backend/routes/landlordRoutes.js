e0d26e00de5d5692cfaed5ff0651f622f1c0f121
Vishv19 <vishvbrahmbhatt19@gmail.com>
2016-05-11 01:55:47 -0700

    Solved the error for looping through enteries of db

--- Backend/routes/landlordRoutes.js
+++ Backend/routes/landlordRoutes.js
@@ -3,11 +3,11 @@
  */
 
 var express = require('express');
+var async = require('async');
 
 var routes = function (connection) {
 
     var landlordRouter = express.Router();
-
 
     landlordRouter.route('/addPlace').post(function (req, res) {
 
@@ -24,7 +24,7 @@ var routes = function (connection) {
         var phone = req.body.place.phone;
         var email = req.body.place.email;
         var description = req.body.place.description;
-
+        var token = req.get('token');
 
         //create a new address row and add it to address table
         var values = {
@@ -35,54 +35,65 @@ var routes = function (connection) {
         };
 
 
-        var query = connection.query('INSERT INTO Address SET ?', values, function (err, results) {
+        var query = connection.query("select user_id from Users where token = ?", [token], function (err, results) {
+        
+            var query2 = connection.query('INSERT INTO Address SET ?', values, function (err2, results2) {
 
 
-            if (err) {
-                return res.json(err);
-            }
-            else {
+                if (err2) {
+                    return res.json(err2);
+                }
+                else {
 
-                //console.log(results[0]);
+                    //console.log(results[0]);
 
-                //get address if from results
+                    //get address if from results
 
-                var addressid = results.insertId;
-
-
-                //address created, now create a place
-                // page_visits_count is 0 as place has just been added.
-                //occupied = 0 for place not being occupied and will
-                //change to 1 when occupied,I think it should be replaced by a boolean
-
-                var values = {
-                    address_id: addressid,
-                    place_name: name,
-                    rooms_count: rooms,
-                    bathrooms_count: bathrooms,
-                    area_squnit: area,
-                    price: price,
-                    phone_number: phone,
-                    email: email,
-                    description: description,
-                    page_visits_count: 0,
-                    occupied: 0
-                };
+                    var addressid = results2.insertId;
 
 
-                var query = connection.query('INSERT INTO Place SET ?', values, function (err, results) {
-                    if (err) {
-                        return res.json(err);
-                    }
-                    else {
-                        return res.json({"result": "true"});
-                    }
-                });
+                    //address created, now create a place
+                    // page_visits_count is 0 as place has just been added.
+                    //occupied = 0 for place not being occupied and will
+                    //change to 1 when occupied,I think it should be replaced by a boolean
+
+                    var values = {
+                        address_id: addressid,
+                        place_name: name,
+                        rooms_count: rooms,
+                        bathrooms_count: bathrooms,
+                        area_squnit: area,
+                        price: price,
+                        phone_number: phone,
+                        email: email,
+                        description: description,
+                        page_visits_count: 0,
+                        occupied: 0
+                    };
 
 
-                //Update the userPlace table
-                //LEFT, NOT DONE AS UNABLE TO TEST
-            }
+                    var query3 = connection.query('INSERT INTO Place SET ?', values, function (err3, results3) {
+                        if (err3) {
+                            return res.json(err3);
+                        }
+                        else {
+                            //Update the userPlace table
+                            userplacevalues = {
+                                user_id: results[0].user_id,
+                                place_id: results3.insertId
+                            };
+                            var query4 = connection.query('INSERT INTO UserPlace SET ?', userplacevalues, function (err4, results4) {
+                                if (err4) {
+                                    return res.json(err4);
+                                }
+                                else {
+                                    return res.json({"result": "true"});
+                                }
+                            });
+                        }
+                    });
+                }
+            });
         });
     });
 
@@ -127,10 +138,7 @@ var routes = function (connection) {
                     }
                 });
             }
-
-
         });
-
     });
 
 
@@ -155,35 +163,26 @@ var routes = function (connection) {
         var query = connection.query('select * from Place where place_id = ?', [placeId], function (err, results) {
 
             if (err) {
+                console.log("Err " + err);
                 return res.json(err);
             } else {
                 var addressid = results[0].address_id;
 
-                var query = connection.query('update Address ' +
-                    'SET street_level = ?' +
-                    ',city_name = ?' +
-                    ',state = ?' +
-                    ',zip_code = ?' +
-                    'where address_id = ?', [street, city, state, zip, addressid], function (err2, results2) {
+                var query = connection.query('update Address SET street_level = ? ,city_name = ?,state = ? ,zip_code = ? where address_id = ?', 
+                    [street, city, state, zip, addressid], function (err2, results2) {
 
 
                     if (err2) {
+                        console.log("Err2 " + err2);
                         return res.json(err);
                     } else {
 
-                        var query = connection.query('update Place ' +
-                            'SET place_name = ?' +
-                            ',rooms_count = ?' +
-                            ',bathrooms_count = ?' +
-                            ',area_squnit = ?' +
-                            ',price = ?' +
-                            ',phone_number = ?' +
-                            ',email = ?' +
-                            ',description = ?' +
-                            'where place_id = ?', [name, rooms, bathrooms, area, price, phone, email, description, placeId], function (err3, results3) {
+                        var query = connection.query( 'update Place SET place_name = ?, rooms_count = ?, bathrooms_count = ? ,area_squnit = ? ,price = ?,phone_number = ?,email = ?,description = ? where place_id = ?'
+                        , [name, rooms, bathrooms, area, price, phone, email, description, placeId], function (err3, results3) {
 
 
                             if (err3) {
+                                console.log("Err3 " + err3);
                                 return res.json(err);
                             } else {
                                 return res.json({"result": "true"});
@@ -239,16 +238,18 @@ var routes = function (connection) {
                         return res.json(err2);
                     } else {
                         //loop through all the results
+                        async.forEachOfSeries(results2, function(placeId, key, next) {
+                            // console.log(placeId.place_id);
 
-                        for (placeId in results2) {
-
-                            var query = connection.query('select * from Place where place_id = ?', [placeId], function (err3, results3) {
+                            var query = connection.query('select * from Place where place_id = ?', [placeId.place_id], function (err3, results3) {
 
                                 if (err3) {
                                     return res.json(err3);
                                 } else {
 
-                                    var addressid = results[0].address_id;
+                                    // console.log(results3);
+                                    var addressid = results3[0].address_id;
+                                    // console.log(addressid);
 
                                     var query = connection.query('select * from Address where address_id = ?', [addressid], function (err4, results4) {
 
@@ -276,15 +277,27 @@ var routes = function (connection) {
                                             }
 
                                             //add the json object to list
+                                            console.log(resObject);
                                             resultObject.list.push(resObject);
+                                            next();
+                                            // console.log(resultObject.list);
                                         }
                                     });
+                                
                                 }
                             });
-                        }
 
-                        //might return wrong response
-                        return res.json(resultObject);
+                        
+                        }, function(looperr){
+                                // if any of the loop processing produced an error, err would equal that error
+                                if( looperr ) {
+                                    // One of the iterations produced an error.
+                                    return res.json(looperr);
+                                } else {
+                                    console.log("everything is processed");
+                                    return res.json(resultObject);
+                                }
+                        });
                     }
                 });
             }
